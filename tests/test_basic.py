9ffd36c30d009d2bc4e7f72f610f43b3dfe0eded
juleshenry <julianplushenry@gmail.com>
2023-02-04 20:13:10 -0600

    What was thought trivial was non(trivial), as usual

---
+++ tests/test_basic.py
@@ -0,0 +1,28 @@
+# import sys
+# from io import StringIO
+# def obtain_std_out(code:str)->str:
+# 	old_stdout = sys.stdout
+# 	redirected_output = sys.stdout = StringIO()
+# 	exec(a_code)
+# 	sys.stdout = old_stdout
+# 	return redirected_output.getvalue()
+import subprocess
+def obtain_std_out(code:str)->str:
+	codeproc = subprocess.Popen(code, stdout=subprocess.PIPE)
+	return(codeproc.stdout.read())
+
+STUB_VARS_GET = ';print("###",vars()'
+print(obtain_std_out('./a.py'))
+# with open('a.py') as a_file, open('b.py') as b:
+# 	a_code = str(a_file.read())
+# 	std_out = obtain_std_out(a_code+STUB_VARS_GET)
+# 	print(a_code+STUB_VARS_GET)
+# 	print(std_out)
+	# try:
+	# 	exec(str(a_file.read()))
+	# except:
+	# 	raise Exception("First file threw an error")
+	# try:
+	# 	exec(str(b_file.read())+';print("###",vars()')
+	# except:
+	# 	raise Exception("Second file threw an error")
