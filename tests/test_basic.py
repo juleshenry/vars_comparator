da803f2d5a1b3289c407de81ec6ab813618338a9
juleshenry <julianplushenry@gmail.com>
2023-02-08 21:03:23 -0600

    Module is the mystery type. Filtering out module-type elements also nontrivial

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,19 @@
 import subprocess, datetime, os,json,ast
 
+VARS_DEFAULT_NAMESPACE = ['__name__',
+'__doc__',
+'__package__',
+'__loader__',
+'__spec__',
+'__annotations__',
+'__builtins__',
+'__file__',
+'__cached__',]
 def yield_vars_stub():
 	class VS:
 		def __init__(s):
 			s.unique_id = '$$$' #str(datetime.datetime.now())
-			s.stub = f'\nprint("{s.unique_id}",vars())'
+			s.stub = f'\nprint("{s.unique_id}");vars())'
 	return VS()
 
 
@@ -39,23 +48,17 @@ def obtain_std_out(code_path: str) -> str:
 
 
 if __name__ == "__main__":
-	x = obtain_std_out('./a.py')
+	# x = obtain_std_out('./a.py')
 	# SUPER BRITTLE
-	y = str(x.stdout).split('$$$')[1][1:-3]
-	print(y)
-	y = ast.literal_eval(y)
-	print(y)
-
-# with open('a.py') as a_file, open('b.py') as b:
-# 	a_code = str(a_file.read())
-# 	std_out = obtain_std_out(a_code+STUB_VARS_GET)
-# 	print(a_code+STUB_VARS_GET)
-# 	print(std_out)
-# try:
-# 	exec(str(a_file.read()))
-# except:
-# 	raise Exception("First file threw an error")
-# try:
-# 	exec(str(b_file.read())+';print("###",vars()')
-# except:
-# 	raise Exception("Second file threw an error")
+	# y = str(x.stdout).split('$$$')[1][1:-3]
+	y = 5
+	x = 'aasdf'
+	"""
+	>>> import a
+	>>> type(a)
+	<class 'module'>
+	"""
+	print([(o[0],type(o[1]),) for o in vars().items() if (o[0] not in VARS_DEFAULT_NAMESPACE) and o[1] not in type()])
+	# print(y)
+	# y = ast.literal_eval(y)
+	# print(y)
